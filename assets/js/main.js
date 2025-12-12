b0a0c5a67ae53c0134ab5d936abeaee4357b3bf3
brfer98 <bruno98fernandez@gmail.com>
2025-12-12 01:14:42 +0100

    added components: footer, header main.js modified: trying to create generic callComponent index.html modified: header and footer components called

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
 // variables
+const header = document.getElementById("header");
 const nav = document.getElementById("navbar");
+const footer = document.getElementById("footer");
 
+const components = [
+    document.getElementById("header"),
+    document.getElementById("navbar"),
+    document.getElementById("footer")
+];
+
+// var component
 
 // functions
+
+function callHeader(){
+    fetch("components/header.html")
+    .then(r => r.text())
+    .then(d => {
+        header.innerHTML = d;
+    })
+}
+
 function callNavbar(){
     fetch("components/navbar.html")
     .then(r => r.text())
@@ -11,6 +29,38 @@ function callNavbar(){
     })
 }
 
+function callFooter(){
+    fetch("components/footer.html")
+    .then(r => r.text())
+    .then(d => {
+        footer.innerHTML = d;
+    })
+}
+
+
+
+
+
+function callComponent(){
+
+    components.forEach(component => {
+        fetch
+        
+    });
+
+
+
+
+    fetch("components/" + component + ".html")
+    .then(r => r.text())
+    .then(d =>{
+        component.innerHTML = d;
+    })
+}
+
 
 // call functions
+document.addEventListener("DOMContentLoaded", callHeader);
 document.addEventListener("DOMContentLoaded", callNavbar);
+document.addEventListener("DOMContentLoaded", callFooter);
+
