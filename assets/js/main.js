62a053db477d63756d747f958cba9054916b1c3c
brfer98 <bruno98fernandez@gmail.com>
2025-12-20 13:38:42 +0100

    main.js -> refactor functions ndex.html -> using grid

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,23 @@
 // variables
-const header = document.getElementById("header");
-const nav = document.getElementById("navbar");
-const footer = document.getElementById("footer");
-
 const components = [
     document.getElementById("header"),
     document.getElementById("navbar"),
     document.getElementById("footer")
 ];
 
-// var component
-
 // functions
-
-function callHeader(){
-    fetch("components/header.html")
-    .then(r => r.text())
-    .then(d => {
-        header.innerHTML = d;
-    })
-}
-
-function callNavbar(){
-    fetch("components/navbar.html")
-    .then(r => r.text())
-    .then(d => {
-        nav.innerHTML = d;
-    })
-}
-
-function callFooter(){
-    fetch("components/footer.html")
-    .then(r => r.text())
-    .then(d => {
-        footer.innerHTML = d;
-    })
-}
-
-
-
-
 
 function callComponent(){
 
-    components.forEach(component => {
-        fetch
-        
+    components.forEach(c => {
+        fetch("components/" + c.id + ".html")
+        .then(r => r.text())
+        .then(d => {
+            c.innerHTML = d;
+        })
     });
-
-
-
-
-    fetch("components/" + component + ".html")
-    .then(r => r.text())
-    .then(d =>{
-        component.innerHTML = d;
-    })
 }
 
 
 // call functions
-document.addEventListener("DOMContentLoaded", callHeader);
-document.addEventListener("DOMContentLoaded", callNavbar);
-document.addEventListener("DOMContentLoaded", callFooter);
-
+document.addEventListener("DOMContentLoaded", callComponent);
